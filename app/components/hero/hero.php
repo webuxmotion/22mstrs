aaf641ec835ed1020bca1743aa1661248c8554f1
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-11 17:43:54 +0300

    Hero

--- app/components/hero/hero.php
+++ app/components/hero/hero.php
@@ -1,12 +1,9 @@
 <div class="hero">
-  <?=$this->component('logo')?>
-  <p class="hero__text">
-    This is StarterKit, based on TonePHP framework.</br>
-    Create pages, components and user interactions.
-  </p>
-  <?=$this->component('button', [
-    "title" => "See docs",
-    "href" => "https://tonephp.com/docs",
-    "target" => "_blank"
-  ])?>
+  <h1>22mstrs</h1>
+
+  <div class="hero__text">
+    <p>
+      
+    </p>
+  </div>
 </div>
--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,8 +10,8 @@ class MainController extends AppController {
     
        $this->setMeta(
            Tone::$app->getProperty('site_name'),
-           'TonePHP Starter KIT',
-           'TonePHP, starter kit'
+           '22mstrs - місце людей та проектів, які змінюють правила гри',
+           '22mstrs, люди, проекти'
        );
     }
 }
